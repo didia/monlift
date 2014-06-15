c2caca1c344dc27718c5ceb17646504cc3d81f4e
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-06-15 15:55:35 -0400

    Adding home page for mobile

--- android_app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++ android_app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/theotherside/monlift_app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/theotherside/monlift_app/platforms/android/CordovaLib/AndroidManifest.xml \
+/Users/didia/Documents/workspace/monlift/android_app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/didia/Documents/workspace/monlift/android_app/platforms/android/CordovaLib/AndroidManifest.xml \
--- android_app/platforms/android/CordovaLib/ant-gen/R.java.d
+++ android_app/platforms/android/CordovaLib/ant-gen/R.java.d
@@ -1,1 +1,1 @@
- : /Users/theotherside/monlift_app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
+ : /Users/didia/Documents/workspace/monlift/android_app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
