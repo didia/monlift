f19ae9cf3141feb20ef6443e426272d2c3717b30
jakeibeemassa <jac.massa0908@gmail.com>
2014-06-16 10:33:28 +0530

    platform configurations
    added android platforms config to the project

--- src/me/didia/monlift/MonliftServlet.java
+++ src/me/didia/monlift/MonliftServlet.java
@@ -8,6 +8,6 @@ public class MonliftServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello,world");
 	}
 }
