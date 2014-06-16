5150ea861c252f5fda99bf7a96f2699f557d315d
jakeibeemassa <jac.massa0908@gmail.com>
2014-06-16 10:33:28 +0530

    platform configurations
    added android platforms config to the project

---
+++ android_app/platforms/android/ant-build/monliftapp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/theotherside/Documents/Projects/monlift/android_app/platforms/android/ant-build/monliftapp-debug-unaligned.apk : \
+/Users/theotherside/Documents/Projects/monlift/android_app/platforms/android/ant-build/monliftapp.ap_ \
+/Users/theotherside/Documents/Projects/monlift/android_app/platforms/android/ant-build/classes.dex \
+/Users/theotherside/Documents/Projects/monlift/android_app/platforms/android/CordovaLib/ant-build/classes.jar \
