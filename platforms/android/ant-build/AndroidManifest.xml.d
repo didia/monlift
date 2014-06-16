9e3c805457a5fe1798b2b64c14caaed60a1884de
jakeibeemassa <jac.massa0908@gmail.com>
2014-06-16 10:33:28 +0530

    platform configurations
    added android platforms config to the project

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/theotherside/Documents/Projects/monlift/android_app/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/theotherside/Documents/Projects/monlift/android_app/platforms/android/AndroidManifest.xml \
