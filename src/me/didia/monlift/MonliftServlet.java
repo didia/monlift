402c1b31b0a03e671b56543ebc918e7f94eeb273
jakeibeemassa <jac.massa0908@gmail.com>
2014-06-18 18:28:19 +0530

    MonliftServlet used as a test servlet
    monlift servlet used as test servlet while implementing tests

--- src/me/didia/monlift/MonliftServlet.java
+++ src/me/didia/monlift/MonliftServlet.java
@@ -1,13 +1,31 @@
 package me.didia.monlift;
 
 import java.io.IOException;
+
 import javax.servlet.http.*;
+
+import me.didia.monlift.helper.EMF;
+import me.didia.monlift.entities.Passenger;
+import me.didia.monlift.entities.User;
+import me.didia.monlift.entities.UserFactory;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
 
 @SuppressWarnings("serial")
 public class MonliftServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello,world");
+		EntityManager em = EMF.getInstance().get().createEntityManager();
+		try{
+			UserFactory uf = UserFactory.getInstance();
+			Passenger p = uf.createPassenger("Jake", "Massa", "jac.masssa0908@gmail.com", "7838073831");
+			em.persist(p);
+		}catch(Exception e){
+			
+		}finally{
+			em.close();
+		}
 	}
 }
