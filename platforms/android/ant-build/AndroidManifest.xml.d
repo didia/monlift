9dc064ed5b2ac3a98df98fa44a954302a27154d1
Trispa <trispa88@gmail.com>
2014-06-21 00:59:26 -0400

    add and configure jersey jar file for app engine

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/theotherside/Documents/Projects/monlift/android_app/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/theotherside/Documents/Projects/monlift/android_app/platforms/android/AndroidManifest.xml \
