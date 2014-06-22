863f97baa884fba459c41f99955c333db1197d90
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-06-22 01:52:46 -0400

    Added listenner for Jesey initialisation

---
+++ src/me/didia/monlift/listenners/InitialisationServlet.java
@@ -0,0 +1,28 @@
+package me.didia.monlift.listenners;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import me.didia.monlift.entities.User;
+
+import com.googlecode.objectify.ObjectifyService;
+
+public class InitialisationServlet implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		registerObjectifyEntities();
+		
+	}
+	private void registerObjectifyEntities()
+	{
+		ObjectifyService.register(User.class);
+	}
+
+}
