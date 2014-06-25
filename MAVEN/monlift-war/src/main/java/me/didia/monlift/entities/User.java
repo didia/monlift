8671789671603cec1e894e6d419da7840194eb4f
jakeibeemassa <jac.massa0908@gmail.com>
2014-06-25 06:30:10 +0530

    maven project added

---
+++ MAVEN/monlift-war/src/main/java/me/didia/monlift/entities/User.java
@@ -0,0 +1,73 @@
+package me.didia.monlift.entities;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+/**
+ * User class
+ * SuperClass to all typer of users in the program
+ * Abstract to avoid being directly used
+ */
+@Entity
+public class User {
+
+	@Id private Long id;
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String phone;
+	private String username;
+	private String password;
+	private boolean isDriver = false;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public boolean isDriver() {
+		return isDriver;
+	}
+	public void setDriver(boolean driver) {
+		this.isDriver = driver;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		
+		return this.password;
+	}
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+}
---
+++ MAVEN/monlift-war/src/test/java/test/didia/monlift/managers/UserManagerTest.java
@@ -0,0 +1,59 @@
+package test.didia.monlift.managers;
+
+import static org.junit.Assert.*;
+import me.didia.monlift.entities.User;
+import me.didia.monlift.factories.DuplicateValueException;
+import me.didia.monlift.factories.UserFactory;
+import me.didia.monlift.managers.UserManager;
+
+import org.junit.Test;
+
+import test.didia.monlift.AbstractTest;
+
+public class UserManagerTest extends AbstractTest {
+
+	/**
+	 * Test of the Create passenger function
+	 */
+	UserManager userManager = UserManager.getInstance();
+	User p;
+	Long id;
+	Long second_id;
+	@Test
+	public void testPromoteToDriver(){
+		String username = "jakeIbee";
+		
+		try {
+			id= userManager.createUser("Jake", "Massa", "jac.massa0908@gmail.com", "7838073831","12345678");
+			userManager.promoteToDriver(id, username);
+			p = userManager.getUser(id);
+			assertEquals(p.getUsername(), username);
+			assertTrue(p.isDriver());
+		} catch (DuplicateValueException e) {
+			
+			fail(e.getMessage());
+		}
+	}
+	@Test
+	public void testPromoteDriverDuplicate()
+	{
+		String username = "TheBlaze";
+		try {
+			id= userManager.createUser("Jake", "Massa", "jac.massa0908@gmail.com", "7838073831","12345678");
+			second_id= userManager.createUser("Jake", "Massa", "jac.massa0904@gmail.com", "7838073831","12345678");
+			userManager.promoteToDriver(id, username);
+			try{
+				userManager.promoteToDriver(second_id, username);
+				fail("Exception DuplicateValueException should be thrown as user is promoted with an existing username");
+			}catch(DuplicateValueException e)
+			{
+				assertTrue(true);
+			}
+		} catch (DuplicateValueException e) {
+			
+			fail(e.getMessage());
+		}
+		
+	}
+
+}
