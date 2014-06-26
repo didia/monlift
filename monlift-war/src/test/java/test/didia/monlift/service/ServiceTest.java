1abb89a51cbbf8eb7f5729edd689dd3a9e069d87
Trispa <trispa88@gmail.com>
2014-06-26 00:10:44 -0400

    convertion inton marven project

---
+++ monlift-war/src/test/java/test/didia/monlift/service/ServiceTest.java
@@ -0,0 +1,29 @@
+package test.didia.monlift.service
+import static org.junit.Assert.*;
+import me.didia.monlift.factories.DuplicateValueException;
+import me.didia.monlift.service.Service;
+
+public class ServiceTest  extends AbstractTest{
+
+	Service myService = new Service();
+	/**
+	 * test of firstnameValidator function
+	 */
+	
+
+	@test
+	
+	public testfirstnameValidator()
+	{
+		try{
+			String fistname = "jo";
+			assertTrue( myService.firstnameValidator(firstname));
+		}
+	} catch (DuplicateValueException e) {
+		
+		fail(e.getMessage());
+	}
+	}
+			
+			
+}
