1d9b8d6f2af45a2f7770194143dce23937d8eab5
jakeibeemassa <jac.massa0908@gmail.com>
2014-06-27 07:55:41 +0530

    creation of do login json generator

--- monlift-war/src/main/java/me/didia/monlift/MonliftServlet.java
+++ monlift-war/src/main/java/me/didia/monlift/MonliftServlet.java
@@ -1,14 +1,16 @@
 package me.didia.monlift;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class MonliftServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-	
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+			throws IOException, ServletException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("login.html");
+		dispatcher.forward(req, resp);
 	}
 }
