e053dab3d18851ed2692d50a1cbb0e4e1f3b45ac
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-06-30 23:59:07 -0400

    Added a ui module whcih will contain all the ui bounded functions

---
+++ www/js/app/ui.js
@@ -0,0 +1,20 @@
+
+
+define(['api/auth'], function(monlift){
+	 
+	 ML = monlift.getInstance();
+	 
+	 return {
+ 		
+		go:function(){
+			// implements all ui specific functions here	
+			
+			
+			
+			
+		
+		}
+		 
+	 }
+ 	
+ })
