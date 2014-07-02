c5d5f0fa4a606f945010ad1b776612fde8bddf8c
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-07-02 09:40:36 -0400

    added the skeletton for the component module which will be used to build all the component for our single page app

---
+++ www/js/app/component.js
@@ -0,0 +1,47 @@
+define(['jquery', 'app/monlift', 'app/auth'], function($, monlift, auth){
+	 
+	 ML = monlift.getInstance();
+	 
+	 return {
+ 		
+		getLoginButton:function(){	
+		
+		},
+		
+		getLogoutButton:function(){
+			
+		},
+		
+		getRegisterButton: function(){
+		
+		},
+		
+		getLoginForm: function(){
+		
+		},
+		
+		getRegisterForm: function(){
+		
+		},
+		
+		getHomePage: function(){
+			
+		},
+		
+		getPassengerHomePage: function(){
+		
+		},
+		
+		getDriverHomePage: function(){
+		
+		},
+		
+		getProfilePage: function()
+		{
+			
+		}
+		
+		 
+	 }
+ 	
+ })
