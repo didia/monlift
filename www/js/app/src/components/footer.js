5f4f34cf70d69e41b701cfa0db76859fb2651c40
Zer0_sec <mbd63@yahoo.fr>
2014-07-05 22:14:41 -0400

    updating footer
    adding new icons

--- www/js/app/src/components/footer.js
+++ www/js/app/src/components/footer.js
@@ -4,14 +4,55 @@ define(['jquery', 'react', 'app/monlift', 'app/auth'], function($, React, monlif
 	var Footer = React.createClass({displayName:'Footer', 
 		render: function(){
 			return (
-        		<div className ="bar bar-tab">
-            		<a className="tab-item">                
-                 		<span className="icon icon-home"  href=""></span>
-            		</a>
-            		<a className="tab-item">
-                  		<span className="icon icon-plus" href="#"></span>
-					</a>
-        		</div>
+        	<div class ="bar bar-tab">
+            
+            <a class="tab-item">
+              
+              <img src="img/Search.png"  href="#" height="30" weight="30" >
+
+              <!-- serach icon  -->
+
+            </a>
+
+          
+            <a class="tab-item">
+
+             <img src="img/addlift.png"  href="#" height="30" weight="30" >
+
+             <!-- add lift -->
+
+            </a>
+
+            <a class="tab-item">
+
+              <img src="img/road.svg"  href="#" height="30" weight="30" >
+
+               <!-- lift or traject-->
+
+            </a>
+
+
+            <a class="tab-item">
+
+
+              <img src="img/notification.png"  href="#" height="30" weight="30" >
+
+               <!-- notification -->
+
+            </a>
+
+
+            <a class="tab-item" href="#">
+
+              <img src="img/profil.png"   height="30" weight="30" >
+
+              <!-- go to profil -->
+
+            </a>
+
+              
+      </div>
+
 			)
 		}
 	});
