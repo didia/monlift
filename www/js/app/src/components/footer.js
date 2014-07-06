4bf6c93ad591b0ea642e567b536eba65c2615364
Zer0_sec <mbd63@yahoo.fr>
2014-07-05 22:16:50 -0400

    stylling the code on footer

--- www/js/app/src/components/footer.js
+++ www/js/app/src/components/footer.js
@@ -4,7 +4,7 @@ define(['jquery', 'react', 'app/monlift', 'app/auth'], function($, React, monlif
 	var Footer = React.createClass({displayName:'Footer', 
 		render: function(){
 			return (
-        	<div class ="bar bar-tab">
+       <div class ="bar bar-tab">
             
             <a class="tab-item">
               
