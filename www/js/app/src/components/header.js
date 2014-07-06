1b7b8bce677ea56e886abb820ec29a87fad59c92
Zer0_sec <mbd63@yahoo.fr>
2014-07-06 00:20:01 -0400

    updating the header

--- www/js/app/src/components/header.js
+++ www/js/app/src/components/header.js
@@ -1,22 +1,55 @@
 /** @jsx React.DOM */
 define(['jquery', 'react', 'app/monlift', 'app/auth'], function($, React, monlift, auth){
-	
-	var Header = React.createClass({displayName:'Header', 
-		render: function(){
-			return (
-				<header class="bar bar-nav">
+		ML = monlift.getInstance();
+		
+		return {	IndexHeader: React.createClass({displayName:'IndexHeader',
+		                    render: function(){
+		                    	    return (
+		                    	    	
+		                    	    	<header class="bar bar-nav">
                                   
-                                  <a class="btn pull-left" href=""> Help </a>
+                                                   <a classeName="btn pull-left" href=""> Help </a>
+                                    
+                                                   <h1 classeName="title">MonLift</h1>
                                   
-                                  <h1 class="title">MonLift</h1>
-                                  
-                                  <a class="btn pull-right" href="html/loginForm.html">Signin </a>
+                                                   <a classeName="btn pull-right" href="html/loginForm.html">Signin </a>
                                 
-                                </header>
-			)
+                                               </header>
+                                               );
+                                               
+                                         }
+                                         
+                                        
+                                        
+                                        
+                                  })
+                                  
+                                  
+                                  
+                                  LoginHeadr : React.createClass({displayName:'LoginHeadr ',
+		                    render: function(){
+		                    	    return (
+		                    	    	
+		                    	    	<header classeName="bar bar-nav">
+                                  
+                                                   <a classeName="btn pull-left" href=""> Help </a>
+                                    
+                                                   <h1 classeName="title">MonLift</h1>
+                                  
+                                                   <a class="btn pull-right" href="html/loginForm.html">Signin </a>
+                                
+                                               </header>
+                                               );
+                                               
+                                         }
+                                         
+                                        
+                                        
+                                        
+                                  })
+			
+			
 		}
-	});
+		
 	
-	return Header;
-	
-});
+})
