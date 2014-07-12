a34b331165e644f839a08712f3f610ac6ba5a187
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-07-11 22:37:04 -0400

    Edited the AuthorizationRequestFilter so it initializes the MonliftContext

--- monlift-war/src/main/java/me/didia/monlift/services/AuthorizationRequestFilter.java
+++ monlift-war/src/main/java/me/didia/monlift/services/AuthorizationRequestFilter.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package me.didia.monlift.services;
 
 /**
@@ -8,29 +5,18 @@ package me.didia.monlift.services;
  *
  */
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
 
 import javax.annotation.Priority;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.ext.Provider;
 
-import me.didia.monlift.securities.AuthentificationErrorException;
-import me.didia.monlift.securities.AuthentificationManager;
-import me.didia.monlift.securities.Session;
-
-import org.apache.commons.io.IOUtils;
-
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
+import me.didia.monlift.MonliftContext;
 
 
 
-
+@Provider
 @Priority(Priorities.AUTHENTICATION)
 public class AuthorizationRequestFilter implements ContainerRequestFilter {
  
@@ -38,36 +24,7 @@ public class AuthorizationRequestFilter implements ContainerRequestFilter {
     public void filter(ContainerRequestContext requestContext)
                     throws IOException {
     	
-    	InputStream inputStream = requestContext.getEntityStream();
-    	StringWriter writer = new StringWriter();
-    	IOUtils.copy(inputStream, writer, "UTF8");
-    	String theString = writer.toString();
-    	if(!theString.startsWith("{"))
-    	{
-    		theString = "{" + theString + "}";
-    	}
-    	String token = null;
-    	
-    	
-    	try {
-			JSONObject json = new JSONObject(theString);
-			token = json.getString("token");
-			if(token != null)
-			{
-				Session session = AuthentificationManager.getInstance().getSession(token);
-				requestContext.setSecurityContext(session);
-			}
-			
-		} catch (JSONException e) {
-			
-			e.printStackTrace();
-		} catch (AuthentificationErrorException e) {
-	
-			requestContext.abortWith(Response
-                    .status(Response.Status.UNAUTHORIZED)
-                    .entity("Invalid token used for logging. " + token)
-                    .build());
-		}
+    	MonliftContext.getInstance().initialize(requestContext);
 		
     	
     }
