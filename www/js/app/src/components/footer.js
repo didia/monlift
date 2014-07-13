b1c2820de9fdfb3f95fe2b0fc71feceae1e5e69e
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-07-13 05:12:17 -0400

    Refactor footer.js and Profile.js so it respects the JSX syntax and compiles

--- www/js/app/src/components/footer.js
+++ www/js/app/src/components/footer.js
@@ -2,102 +2,56 @@
 define(['jquery', 'react', 'app/monlift', 'app/auth'], function($, React, monlift, auth){
 		ML = monlift.getInstance();
 	
-		render{
+		return {
 			
 				IndexFooter: React.createClass({displayName:'IndexFooter',
 				
-				render: function(){
-					return (
+					render: function(){
+						return (
 	                  
-	                  <div class ="bar bar-tab">
-					
-					<a class="tab-item">
-					  
-						<img src="img/Search.png"  href="#" height="30" weight="30" />
-		
-					</a>
-		
-				  
-					<a class="tab-item">
-		
-						<img src="img/addlift.png"  href="#" height="30" weight="30" />
-
-		
-					</a>
-		
-					<a class="tab-item">
-		
-					  <img src="img/road.svg"  href="#" height="30" weight="30"  />
-		
-					</a>
-		
-		
-					<a class="tab-item">
-		
-		
-					  <img src="img/notification.png"  href="#" height="30" weight="30" />
-		
-		
-					</a>
-		
-		
-					<a class="tab-item" href="#">
-		
-					  <img src="img/profil.png"   height="30" weight="30"  />
-	
-					</a>
-		
-					  
-			  </div>
-
-                    );
+						  <div className ="bar bar-tab">
+							<a className="tab-item">
+								<img src="img/Search.png"  href="#" height="30" weight="30" />
+							</a>
+							<a className="tab-item">
+								<img src="img/addlift.png"  href="#" height="30" weight="30" />
+							</a>
+							<a className="tab-item">
+								<img src="img/road.svg"  href="#" height="30" weight="30"  />
+							</a>
+							<a className="tab-item">
+								<img src="img/notification.png"  href="#" height="30" weight="30" />
+							</a>
+							<a className="tab-item" href="#">
+								<img src="img/profil.png"   height="30" weight="30"  />
+							</a>
+						  </div>
+                    	);
                                                
-                }
+               		 }
    
-                                        
-      })
-		           DriverAccountFooter: React.createClass({displayName:'DriverAccountFooter',
-				
-			    render: function(){
-			            return (
-						
-		              <div class ="bar bar-tab">
-                                  
-                                  <a class="tab-item" href="../index.html">
-                                  
-                                  <img src="img/Search.png"   height="30" weight="30" >
-                                  
-                                  </a>
-                                  
-                                  <a class="tab-item" >
-                                  
-                                  <img src="img/addlift.png" height="30" weight="30" >
-                                  
-                                  </a>
-                                  
-                                  
-                                  <a class="tab-item">
-                                  
-                                  <img src="img/road.svg"  href="#" height="30" weight="30" >
-                                  
-                                  </a>
-                               
-                               </div>
-
-       
-
-
-						
+             	 }),
+		           
+				 DriverAccountFooter: React.createClass({displayName:'DriverAccountFooter',
+					render: function(){
+						return (
+							<div className ="bar bar-tab">
+								<a className="tab-item" href="../index.html">
+									<img src="img/Search.png"   height="30" weight="30" />
+								</a> 
+								<a className="tab-item" >
+									<img src="img/addlift.png" height="30" weight="30" />
+								</a>
+								<a className="tab-item">
+									<img src="img/road.svg"  href="#" height="30" weight="30" />
+								</a>
+							</div>
 						);
-						
-				     }
-						
-		         })
+							
+					}
+							
+				})
 	
-		
-		
-		
-		
 			
 	}
 
