321c7915c9a290b372a22e906c2e2455f5120883
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-07-13 13:32:12 -0400

    Edit the header so it takes buttons directly without passing by the component.js

--- www/js/app/src/components/header.js
+++ www/js/app/src/components/header.js
@@ -1,40 +1,78 @@
 /** @jsx React.DOM */
-define(['jquery', 'react', 'app/monlift', 'app/auth'], function($, React, monlift, auth){
+define(['jquery', 'react', 'app/monlift', 'app/auth','components/buttons'], 
+function($, React, monlift, auth, buttons){
+		
 		ML = monlift.getInstance();
 		
-		return {	
-			IndexHeader: React.createClass({displayName:'IndexHeader',
+		
+		return {
+			Header: React.createClass({displayName:'Header',
+				
 		    	render: function(){
+					var Button = null;
+					switch(this.props.page)
+					{
+						case 'index':
+						case 'home':
+							Button = ML.isUserLoggedIn()?buttons.LogoutButton:buttons.LoginButton;
+							break;
+						case 'login':
+							Button = buttons.RegisterButton;
+							break;
+						case 'register':
+							Button = buttons.LoginButton;
+							break;
+						default:
+							Button = ML.isUserLoggedIn()?buttons.LogoutButton:buttons.LoginButton;
+							break;
+							
+					}
+					
 		        	return (
 		            	<header className="bar bar-nav">
                         	<a className="btn pull-left" href=""> Help </a>
                             <h1 className="title">MonLift</h1>
-							<a className="btn pull-right" id="signin">Signin </a>
+							{Button != null?<Button />:''}
                         </header>
                     );
                                                
               	 }
-   
-                                        
+                        
+			}),
+				
+			IndexHeader: React.createClass({displayName:'IndexHeader',
+		    	render: function(){
+					var Button = ML.isUserLoggedIn()?buttons.LogoutButton:buttons.LoginButton;
+		        	return (
+		            	<header className="bar bar-nav">
+                        	<a className="btn pull-left" href=""> Help </a>
+                            <h1 className="title">MonLift</h1>
+							<Button />
+                        </header>
+                    );
+                                               
+              	 }
+                        
 			}),
                                   
 			LoginHeader : React.createClass({displayName:'LoginHeader',
 		    	render: function(){
+					var Button = component.getRegisterButton();
 		        	return (
 						<header className="bar bar-nav">
 							<h1 className="title">MonLift</h1>
-							<a className="btn pull-right" href="">logout</a>
+							<Button />
                          </header>
                     );
                                                
                 }
             }),
-            logoutHeader: react.createClass({displayName:'logoutHeader'
+            LogoutHeader: React.createClass({displayName:'LogoutHeader',
             	render:function(){
             		return(
             				<header className = "bar bar-nav">
             				<h1 className = "title">Monlift</h1>
-            				<a className = "btn pull-right" href ="#" Login</a>
+            				<a className = "btn pull-right" href ="#"> Login</a>
             				</header>
             				);
             	}
