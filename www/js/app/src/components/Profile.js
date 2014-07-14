e2e305d4d08afa39e2d50094f8ac21afff5ca74a
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-07-14 17:35:57 -0400

    Refactoring of profile.js and change the name of the file  to lowercase

--- www/js/app/src/components/Profile.js
+++ www/js/app/src/components/Profile.js
@@ -46,35 +46,24 @@ define(['jquery', 'react', 'app/monlift'], function($, React, monlift){
            PassangerProfile: React.createClass({displayName:'PassangerProfile',
                 	render: function(){
                 	        return (
-                	        
-                                      <div className="content_Passanger_Profil">
-
-
+								<div className="content_Passanger_Profil">
                                         <div id="box">
-
-                                          <div id="topleft">
-                                             
+                                          
+										  <div id="topleft">
                                              <img src="img/profil.png"   height="30" weight="30" id="profil" href="" />
                                              <h5 id="MyInfos">My Infos</h5>
-
                                           </div>
+                                        
+										<div id="topright">
+                                         	<img src="img/addlift.png"   height="30" weight="30" id="addlift" href="" />
+                                         	<h5 id="MyAutos">My Autos</h5>
+                                        </div>
 
-                                         <div id="topright">
-                                         
-                                         <img src="img/addlift.png"   height="30" weight="30" id="addlift" href="" />
-                                         <h5 id="MyAutos">My Autos</h5>
-  
-                                         </div>
-
-                                         <div id="bottomleft">
-                                         <img src="img/Alert.png"   height="30" weight="30" id="Alert" href="" />
-                                         <h5 id="MyAlerts">My Alerts</h5>
-
-
-                                         </div>
-
-
-
+                                        <div id="bottomleft">
+                                         	<img src="img/Alert.png"   height="30" weight="30" id="Alert" href="" />
+                                         	<h5 id="MyAlerts">My Alerts</h5>
+                                        </div>
+									
                                       </div>
 
                                     </div>        
