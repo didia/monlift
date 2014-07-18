88f30e7100a25974362d572361a50c466c5b82aa
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-07-18 17:22:44 -0400

    Use constants to refer to user attributes

---
+++ monlift-war/src/main/java/me/didia/monlift/factories/UserAttributes.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package me.didia.monlift.factories;
+
+/**
+ * @author didia
+ *
+ */
+public class UserAttributes {
+	
+	public static final String FIRSTNAME = "firstname";
+	public static final String LASTNAME = "lastname";
+	public static final String EMAIL = "email";
+	public static final String PASSWORD = "password";
+	public static final String PHONE = "phone";
+	public static final String USERNAME = "username";
+	public static final String ISDRIVER = "isdriver";
+	
+}
