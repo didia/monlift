076656b18055dde9f9972503208063ad0b9a708c
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-07-23 21:01:44 -0400

    Added fonctions to get multiple mock users at once

--- monlift-war/src/test/java/test/didia/monlift/MockFactory.java
+++ monlift-war/src/test/java/test/didia/monlift/MockFactory.java
@@ -3,6 +3,8 @@
  */
 package test.didia.monlift;
 
+import java.util.ArrayList;
+
 import me.didia.monlift.entities.Car;
 import me.didia.monlift.entities.User;
 import me.didia.monlift.factories.DuplicateValueException;
@@ -36,7 +38,24 @@ public class MockFactory {
 			return null;
 		}
 	}
-	
+	public static ArrayList<User> getMultipleUser(int number)
+	{
+		ArrayList<User> users = new ArrayList<User>(number);
+		
+		ArrayList<RegisterRequest> requests = getMultipleRegisterRequest(number);
+		for(RegisterRequest request : requests)
+		{
+			try{
+				users.add(UserManager.createUser(request));
+			}catch(DuplicateValueException e){
+				e.printStackTrace();
+				users.add(null);
+			}
+			
+		}
+		
+		return users;
+	}
 	public static Car getCar()
 	{
 		CreateCarRequest request = getCreateCarRequest();
@@ -52,16 +71,42 @@ public class MockFactory {
 	}
 	
 	public static RegisterRequest getRegisterUserRequest(){
-		RegisterRequest request = new RegisterRequest();
-		request.setEmail(MOCK_USER_EMAIL);
-		request.setFirstname(MOCK_USER_FIRSTNAME);
-		request.setLastname(MOCK_USER_LASTNAME);
-		request.setPassword(MOCK_USER_PASSWORD);
-		request.setPhone(MOCK_USER_NUMBER);
-		
-		return request;
-		
+		return getRegisterRequest(MOCK_USER_FIRSTNAME, MOCK_USER_LASTNAME, 
+				                  MOCK_USER_PASSWORD, MOCK_USER_NUMBER);
 		
 	}
 	
+	public static ArrayList<RegisterRequest> getMultipleRegisterRequest(int number){
+		
+		ArrayList<RegisterRequest> requests = new ArrayList<RegisterRequest>(number);
+		
+		RegisterRequest request = getRegisterUserRequest();
+		requests.add(request);
+		for(int i=0; i<number; i++)
+		{
+			String firstname = request.getFirstname() + "-"+ i;
+			String lastname = request.getLastname() + "-" + i;
+			String password = request.getPassword() + "-" + i;
+			String phone = request.getPhone() + "-" + i;
+			RegisterRequest aRequest = getRegisterRequest(firstname, lastname,
+														  password, phone);
+			requests.add(aRequest);
+			
+		}
+		
+		return requests;
+	}
+	
+	private static RegisterRequest getRegisterRequest(String p_firstname, String p_lastname, String p_password, String p_phone)
+	{
+		RegisterRequest request = new RegisterRequest();
+		request.setFirstname(p_firstname);
+		request.setLastname(p_lastname);
+		request.setPassword(p_password);
+		request.setPhone(p_phone);
+		String email = request.getFirstname() + "." + request.getLastname() + "@example.com";
+		request.setEmail(email);
+		
+		return request;
+	}
 }
