8cfd2e5c12bc2d497e42e7cc4438c42a9ae5c32b
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-08-02 22:24:06 -0400

    Added LiftService

---
+++ monlift-war/src/main/java/me/didia/monlift/services/LiftService.java
@@ -0,0 +1,64 @@
+package me.didia.monlift.services;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import me.didia.monlift.MonliftContext;
+import me.didia.monlift.entities.Lift;
+import me.didia.monlift.factories.DuplicateValueException;
+import me.didia.monlift.managers.LiftManager;
+import me.didia.monlift.requests.CreateLiftRequest;
+import me.didia.monlift.responses.LiftResponse;
+
+/**
+ * @author didia
+ *
+ */
+@Path("/lifts")
+public class LiftService {
+	
+	@POST
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public List<LiftResponse> getAllLifts(){
+		
+		return null;
+		
+	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public LiftResponse getLift(@PathParam("liftId") Long p_liftId){
+		Lift lift = LiftManager.getLift(p_liftId);
+		LiftResponse liftResponse = new LiftResponse();
+		liftResponse.build(lift);
+		
+		return liftResponse;
+		
+	}
+	
+	@POST
+	@Path("/create")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public LiftResponse createLift(CreateLiftRequest p_request) throws DuplicateValueException{
+		p_request.validate();
+		Lift lift = LiftManager.createLift(MonliftContext.getInstance().getCurrentUser(), p_request);
+		LiftResponse response = new LiftResponse();
+		response.build(lift);
+		return response;
+	}
+
+	
+
+}
