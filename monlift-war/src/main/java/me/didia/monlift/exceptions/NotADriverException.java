4af851cb47d1cad31e4dc5693adb281057e9975a
Aristote Diasonama <aristotediasonama@yahoo.fr>
2014-08-10 20:14:06 -0400

    Added method create for the car service
    Added an endpoint to create a car

---
+++ monlift-war/src/main/java/me/didia/monlift/exceptions/NotADriverException.java
@@ -0,0 +1,10 @@
+package me.didia.monlift.exceptions;
+
+@SuppressWarnings("serial")
+public class NotADriverException extends MonliftException {
+
+	public NotADriverException(String message) {
+		super(message);
+	}
+
+}
--- monlift-war/src/main/java/me/didia/monlift/services/CarService.java
+++ monlift-war/src/main/java/me/didia/monlift/services/CarService.java
@@ -1,12 +1,43 @@
-/**
- * 
- */
+
 package me.didia.monlift.services;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import me.didia.monlift.MonliftContext;
+import me.didia.monlift.entities.Car;
+import me.didia.monlift.entities.User;
+import me.didia.monlift.exceptions.DuplicateValueException;
+import me.didia.monlift.exceptions.MonliftException;
+import me.didia.monlift.exceptions.NotADriverException;
+import me.didia.monlift.managers.LiftManager;
+import me.didia.monlift.managers.UserManager;
+import me.didia.monlift.requests.CreateCarRequest;
+import me.didia.monlift.responses.CarResponse;
 
 /**
  * @author didia
  *
  */
+@Path("/cars")
 public class CarService {
-
+	
+	@POST
+	@Path("/create")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public CarResponse createCar(CreateCarRequest p_request) throws MonliftException{
+		User user = MonliftContext.getInstance().getCurrentUser();
+		if (!user.isDriver()){
+			throw new NotADriverException("User must be a driver to add a car");
+		}
+		Car car = LiftManager.createCar(user, p_request);
+		CarResponse response = new CarResponse();
+		response.build(car);
+		
+		return response;
+	}
 }
